2fbfe22b1ef992ca52792ec77fc0bf567666b335
amb7316 <56976617+amb7316@users.noreply.github.com>
2020-12-11 21:42:22 -0500

    Added basic character movement

---
+++ Game PaJama Party Game/Assets/Scripts/Player.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public CharacterController2D characterController;
+
+    public float moveSpeed = 40f;
+    private float horizontal;
+
+    private bool jump = false;
+    private bool crouch = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+      
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontal = Input.GetAxisRaw("Horizontal") * moveSpeed;
+
+        if(Input.GetButtonDown("Jump"))
+        {
+            jump = true;
+        }
+
+        if(Input.GetButtonDown("Crouch"))
+        {
+            crouch = true;
+        }
+
+        else if(Input.GetButtonUp("Crouch"))
+        {
+            crouch = false;
+        }
+        
+    }
+
+    void FixedUpdate()
+    {
+        characterController.Move(horizontal * Time.fixedDeltaTime, crouch, jump);
+        jump = false;
+    }
+}
