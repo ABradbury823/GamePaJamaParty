8309c0e7a1eff43af0dc8649d523cc90e404827e
meo6332 <60484806+meo6332@users.noreply.github.com>
2020-12-12 23:30:30 -0500

    Made the PressButton Script press and disable the door
    PressButton now works at a basic level with disabling the door associated with it, along with the button being moved when it is pressed.

--- Game PaJama Party Game/Assets/Scripts/PressButton.cs
+++ Game PaJama Party Game/Assets/Scripts/PressButton.cs
@@ -7,11 +7,15 @@ public class PressButton : MonoBehaviour
 
     public GameObject box;
     public GameObject door;
+    public GameObject buttonParent;
     private bool pressed = false;
     public static Vector3 originalPos;
+    private float rayHeight = 0.35f;
 
-    // Vectors to move the button
-    private Vector3 downShift = new Vector3(0f, -0.1f);
+
+    // Vectors to move the button and cast rays
+    private Vector3 downShift = new Vector3(0f, -0.05f);
+    
 
     // Start is called before the first frame update
     void Start()
@@ -22,24 +26,62 @@ public class PressButton : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        Debug.DrawRay(transform.position, Vector2.up * rayHeight);
+        RaycastHit2D hit = Physics2D.Raycast(originalPos, Vector2.up, rayHeight);
+
+
+        // Consider adding more RaycastHit2D variables with an adjusted first variable
+        // to cover more of the button's width.
+        if (hit.collider != null)
+        {
+            if (!pressed) // Check if the button was already pressed
+            {
+                boxContact();
+            }
+            
+        }
+        else // Nothing is on top of the button
+        {
+            boxRelease();
+        }
+
     }
+
+    /*
+    public void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (!collision.transform.IsChildOf(buttonParent.transform))
+        {
+            boxContact();
+        }
+    }
+
+    public void OnCollisionExit2D(Collision2D collision)
+    {
+        if (!collision.transform.IsChildOf(buttonParent.transform))
+        {
+            boxRelease();
+        }
+    }
+    */
 
     void boxContact()
     {
-        if (!pressed) // Check if the button was already pressed
+        if (!pressed)
         {
             pressed = true;
-            transform.position = transform.position + downShift;
+            transform.position = originalPos + downShift;
+            door.SetActive(false);
         }
     }
 
     void boxRelease()
     {
-        if (pressed) // Check to see if the button was already pressed
+        if (pressed)
         {
             pressed = false;
             transform.position = originalPos;
+            door.SetActive(true);
         }
     }
 }
