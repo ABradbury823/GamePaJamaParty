11e0029e5a79e1fa0f05f891401d1888bc9c46f8
meo6332 <60484806+meo6332@users.noreply.github.com>
2020-12-12 22:07:59 -0500

    Progress on Press Button
    Changed the press button function to have some functionality for checking if a button is pressed.

--- Game PaJama Party Game/Assets/Scripts/PressButton.cs
+++ Game PaJama Party Game/Assets/Scripts/PressButton.cs
@@ -4,15 +4,42 @@ using UnityEngine;
 
 public class PressButton : MonoBehaviour
 {
+
+    public GameObject box;
+    public GameObject door;
+    private bool pressed = false;
+    public static Vector3 originalPos;
+
+    // Vectors to move the button
+    private Vector3 downShift = new Vector3(0f, -0.1f);
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        originalPos = transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    void boxContact()
+    {
+        if (!pressed) // Check if the button was already pressed
+        {
+            pressed = true;
+            transform.position = transform.position + downShift;
+        }
+    }
+
+    void boxRelease()
+    {
+        if (pressed) // Check to see if the button was already pressed
+        {
+            pressed = false;
+            transform.position = originalPos;
+        }
     }
 }
